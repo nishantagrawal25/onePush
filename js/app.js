df332810fe5917458f6cfccb28a5c53c36d8666c
Nishant Agrawal <nishant.agrawal@industrybuying.com>
2016-09-17 18:26:39 +0530

    Implemented like feature for respective url using local storage

--- js/app.js
+++ js/app.js
@@ -8,7 +8,12 @@ app.controller('AdminCtrl', [ '$scope', 'AdminSvc', '$location','$http', functio
   function getData() {
     AdminSvc.getData().then(function(data) {
       $scope.websiteData = data.websites;
-      console.log($scope.websiteData);
+      angular.forEach($scope.websiteData, function(itm) {
+        itm.likes = localStorage.getItem(itm.url_address);
+        if (itm.likes == null) {
+          itm.likes = 0;
+        }
+      })
     })
   }
   
@@ -18,6 +23,16 @@ app.controller('AdminCtrl', [ '$scope', 'AdminSvc', '$location','$http', functio
     }, function(err) {
       console.log("failure")  
     })
+  }
+  
+  $scope.like = function(data) {
+    data.likes = 0;
+    data.likes = localStorage.getItem(data.url_address);
+    if (data.likes == null) {
+      data.likes = 0;
+    }
+    data.likes = parseInt(data.likes) + 1;
+    localStorage.setItem(data.url_address, data.likes);
   }
 
 } ]);
